3922591d8c882a70517b752ae95dadffd9ec671d
Oliver Ceccopieri <oliverbiotech@gmail.com>
2024-05-17 18:49:32 -0400

    Add all source files

---
+++ app.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+
+import datetime as dt
+import os
+import re
+import subprocess
+
+from gooey import Gooey, GooeyParser, local_resource_path
+
+IMAGES_PATH = local_resource_path('images')
+FFMPEG_PATH = local_resource_path('ffmpeg-7.0-essentials_build/bin/ffmpeg.exe')
+
+
+def parse_time(timestring):
+
+    time_formats = [
+        '%H:%M:%S.%f',
+        '%H:%M:%S',
+        '%M:%S.%f',
+        '%M:%S',
+        '%S.%f',
+        '%S',
+    ]
+
+    for tform in time_formats:
+        try:
+            return dt.datetime.strptime(timestring,tform)
+        except ValueError:
+            pass
+    raise Exception(f'Could not parse time string: "{timestring}"')
+
+
+def process_ff_output(ffprocess,
+                      clip_duration,
+                      part = 0,
+                      total_parts = 2,
+                      time_regex = re.compile(r'time=\s*([\d\:\.]+)\s+'),
+                      zero_time = dt.datetime(1900, 1, 1, 0, 0)):
+
+    for line in ffprocess.stdout:
+        print(line.rstrip())
+
+        if match := time_regex.search(line):
+            timecode = match.groups()[0]
+            delta = parse_time(timecode) - zero_time
+            progress = clip_duration * part + delta.total_seconds()
+            print(f'PROGRESS: {int(progress)}/{int(clip_duration * total_parts)}')
+
+    ffprocess.wait()
+    if ffprocess.returncode:
+        raise Exception('Error in ffmpeg pass 1')
+
+
+def convert(input_filename,
+            output_filename,
+            clip_from,
+            clip_to,
+            scale_width,
+            scale_height,
+            framerate,
+            aspect_crop,
+            aspect_width,
+            aspect_height,
+            target_video_size_mb,
+            encoding_speed,
+            audio_bitrate_kbps,
+            output_format,
+            video_lib,
+            audio_lib):
+
+    clip_start_time = parse_time(clip_from)
+    clip_end_time = parse_time(clip_to)
+    clip_duration = (clip_end_time - clip_start_time).total_seconds()
+    desired_bitrate_kbps = (target_video_size_mb * 8192) / (int(clip_duration) + 1)
+    target_video_bitrate_kbps = desired_bitrate_kbps - audio_bitrate_kbps
+
+    crop_string = ''
+    if aspect_crop:
+        crop_string = f'crop=ih/{aspect_height}*{aspect_width}:ih,'
+
+    print('working dir:', os.getcwd())
+    print('clip duration:', clip_duration)
+    print('video bitrate:', target_video_bitrate_kbps)
+
+    ffprocess = subprocess.Popen([
+        FFMPEG_PATH,
+        '-y',
+        '-i', input_filename,
+        '-ss', clip_from,
+        '-to', clip_to,
+        '-r', f'{framerate}',
+        '-vf', f'{crop_string}scale={scale_width}:{scale_height}',
+        '-c:v', video_lib,
+        '-b:v', f'{target_video_bitrate_kbps}k',
+        '-preset', encoding_speed,
+        '-c:a', audio_lib,
+        '-b:a', f'{audio_bitrate_kbps}k',
+        '-f', output_format,
+        '-pass', '1',
+        'NUL'
+    ],
+    stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    stdin=subprocess.DEVNULL, shell=True, universal_newlines=True)
+
+    print(ffprocess.args)
+    process_ff_output(ffprocess, clip_duration, part=0)
+
+    ffprocess = subprocess.Popen([
+        FFMPEG_PATH,
+        '-y',
+        '-i', input_filename,
+        '-ss', clip_from,
+        '-to', clip_to,
+        '-r', f'{framerate}',
+        '-vf', f'{crop_string}scale={scale_width}:{scale_height}',
+        '-c:v', video_lib,
+        '-b:v', f'{target_video_bitrate_kbps}k',
+        '-preset', encoding_speed,
+        '-c:a', audio_lib,
+        '-b:a', f'{audio_bitrate_kbps}k',
+        '-f', output_format,
+        '-pass', '2',
+        output_filename
+    ],
+    stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    stdin=subprocess.DEVNULL, shell=True, universal_newlines=True)
+
+    print(ffprocess.args)
+    process_ff_output(ffprocess, clip_duration, part=1)
+
+
+@Gooey(
+    program_name='Zutano\'s Video Converter Script v1.5',
+    image_dir=IMAGES_PATH,
+    default_size=(800, 850),
+    progress_regex=r'^PROGRESS: (?P<current>\d+)/(?P<total>\d+)$',
+    progress_expr='current / total * 100',
+    hide_progress_msg=True,
+    clear_before_run=True
+)
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    parser = GooeyParser(description="Convert a video to the desired file size")
+
+    parser.add_argument(
+        'input_filename', metavar='Input File', type=str,
+        help='Video file to convert',
+        widget='FileChooser',
+        gooey_options={
+            'wildcard': "MP4 (*.mp4)|*.mp4|All files (*.*)|*.*",
+        }
+    )
+    parser.add_argument(
+        'output_filename', metavar='Output File', type=str,
+        help='Output file name',
+        widget='FileSaver',
+        gooey_options={
+            'wildcard': "MP4 (*.mp4)|*.mp4|All files (*.*)|*.*",
+        }
+    )
+    parser.add_argument(
+        'clip_from', metavar='Clip Start (MM:SS)', type=str,
+        help='Starting timepoint to clip in the format MM:SS',
+        default='00:00'
+    )
+    parser.add_argument(
+        'clip_to', metavar='Clip End (MM:SS)', type=str,
+        help='Ending timepoint to clip in the format MM:SS',
+        default='00:30'
+    )
+
+    advanced_settings = parser.add_argument_group(
+        'Advanced Settings',
+        gooey_options={'show_border': True}
+    )
+    advanced_settings.add_argument(
+        'resolution', metavar='Resolution', type=str,
+        help='Output video resolution',
+        choices=['480p', '720p', '1080p'], default='1080p'
+    )
+    advanced_settings.add_argument(
+        'framerate', metavar='Frame Rate', type=int,
+        help='Output video frames per second',
+        default=60
+    )
+    advanced_settings.add_argument(
+        'aspect', metavar='Aspect Ratio', type=str,
+        help='Output video aspect ratio',
+        choices=['Original', '16:9', '21:9'], default='Original'
+    )
+    advanced_settings.add_argument(
+        'target_size', metavar='Target File Size (MB)', type=float,
+        help='Desired size of the output video file in megabytes',
+        default=25
+    )
+    advanced_settings.add_argument(
+        'encoding_speed', metavar='Encoding Speed', type=str,
+        help='Encoding speed preset (slowest for best quality)',
+        choices=[
+            'ultrafast',
+            'superfast',
+            'veryfast',
+            'faster',
+            'fast',
+            'medium',
+            'slow',
+            'slower',
+            'veryslow',
+        ], default='veryslow'
+    )
+    advanced_settings.add_argument(
+        'format', metavar='Output File Format', type=str,
+        help='Format of the output video file',
+        choices=['mp4'], default='mp4'
+    )
+    advanced_settings.add_argument(
+        'audio_bitrate', metavar='Audio Bitrate (kbps)', type=int,
+        help='Output audio bitrate in kilobits per second',
+        default=128
+    )
+    advanced_settings.add_argument(
+        'video_lib', metavar='Video Encoder', type=str,
+        help='Encoder library for output video',
+        choices=['libx264'], default='libx264'
+    )
+    advanced_settings.add_argument(
+        'audio_lib', metavar='Audio Encoder', type=str,
+        help='Encoder library for output audio',
+        choices=['aac'], default='aac'
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    height = {
+        '480p': 480,
+        '720p': 720,
+        '1080p': 1080,
+    }[args.resolution]
+
+    should_crop = (args.aspect != 'Original')
+    aspect_width, aspect_height = {
+        'Original': (0, 0),
+        '16:9': (16, 9),
+        '21:9': (21, 9),
+    }[args.aspect]
+
+    convert(
+        args.input_filename,
+        args.output_filename,
+        args.clip_from,
+        args.clip_to,
+        -2,
+        height,
+        args.framerate,
+        should_crop,
+        aspect_width,
+        aspect_height,
+        args.target_size,
+        args.encoding_speed,
+        args.audio_bitrate,
+        args.format,
+        args.video_lib,
+        args.audio_lib
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ build.spec
@@ -0,0 +1,45 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+import gooey
+gooey_root = os.path.dirname(gooey.__file__)
+
+block_cipher = None
+
+image_overrides = Tree('images', prefix='images')
+
+options = [
+    ('u', None, 'OPTION'),
+    ('W ignore', None, 'OPTION'),
+]
+
+a = Analysis(['app.py'],  # replace me with your path
+             pathex=['app.py'],
+             binaries=[('ffmpeg-7.0-essentials_build/bin/ffmpeg.exe', 'ffmpeg-7.0-essentials_build/bin')],
+             datas=[('images', 'images')],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          options,
+          name='zutano_videoconverter_v1.5',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          upx_exclude=[],
+          runtime_tmpdir=None,
+          console=False,
+          icon=os.path.join('images', 'program_icon.png'))
